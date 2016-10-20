18d053524b469bff5b2da1f965af9be1dc1ad86a
KurtWeber <weberkrt@msn.com>
2016-10-19 22:58:20 -0400

    Kind of Responsive

--- Content/JavaScript.js
+++ Content/JavaScript.js
@@ -6,11 +6,11 @@
     //Generate random numbers between 1-20
     var num = Math.floor((Math.random() * 19) + 1);
     
-    //Array of actual Magic 8 Ball responces
+    //Array of actual Magic 8 Ball responses
     var statement = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
    
     
-    //display responces to Magic 8 Ball
+    //display responses to Magic 8 Ball
     document.getElementById("text").innerHTML = statement[num - 1]
        
 }
